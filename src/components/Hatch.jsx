7ed7137ecd7dd041174a303dcd4b02360c2aa270
[satuhalinen] <satuh9@gmail.com>
2024-04-02 16:06:24 +0300

    add dropdowns for hatches

--- src/components/Hatch.jsx
+++ src/components/Hatch.jsx
@@ -1,3 +1,6 @@
+import { DropdownButton } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
+
 function Hatch({ number }) {
   return (
     <div
@@ -9,6 +12,18 @@ function Hatch({ number }) {
       }}
     >
       <div className="hatch">{number}</div>
+      <DropdownButton id="dropdown-item-button" title="Choose a topic">
+        <Dropdown.Item as="button">Adults</Dropdown.Item>
+        <Dropdown.Item as="button">Animals</Dropdown.Item>
+        <Dropdown.Item as="button">Children and teenagers</Dropdown.Item>
+        <Dropdown.Item as="button">Elderly</Dropdown.Item>
+      </DropdownButton>
+      <DropdownButton id="dropdown-item-button" title="Choose an alternative">
+        <Dropdown.Item as="button">Alternative 1</Dropdown.Item>
+        <Dropdown.Item as="button">Alternative 2</Dropdown.Item>
+        <Dropdown.Item as="button">Alternative 3</Dropdown.Item>
+        <Dropdown.Item as="button">Alternative 4</Dropdown.Item>
+      </DropdownButton>
     </div>
   );
 }
