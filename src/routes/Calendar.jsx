1e87dedfcdba725e58226834350170fcc418faa5
[satuhalinen] <satuh9@gmail.com>
2024-04-02 14:44:33 +0300

    add skeleton calendar

--- src/routes/Calendar.jsx
+++ src/routes/Calendar.jsx
@@ -1,3 +1,33 @@
-export default function Calendar() {
-  return <div>Calendar</div>;
+import Hatch from "../components/Hatch";
+import "../calendar.css";
+import { Card } from "react-bootstrap";
+import homeSymbol from "../public/home.png";
+
+function Calendar() {
+  return (
+    <div className="calendarSections" style={{ display: "flex" }}>
+      <div className="calendar" style={{ border: "1px solid black" }}>
+        {Array.from({ length: 31 }).map((_, i) => (
+          <Hatch key={i} number={i + 1} />
+        ))}
+      </div>
+      <Card
+        className="gamification"
+        style={{
+          display: "grid",
+          width: "30%",
+          border: "1px solid black",
+          height: "500px",
+        }}
+      >
+        <Card.Body>
+          <Card.Title>Gamification</Card.Title>
+          <Card.Title>Score</Card.Title>
+        </Card.Body>
+        <Card.Img variant="top" src={homeSymbol} />
+      </Card>
+    </div>
+  );
 }
+
+export default Calendar;
