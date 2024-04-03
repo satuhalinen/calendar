07acbf01e0f6b80023d46c7a2f032e3e3b3d39f9
BenPet <bendzuke@gmail.com>
2024-04-03 22:07:03 +0300

    login skeleton

---
+++ src/routes/login/Login.jsx
@@ -0,0 +1,28 @@
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <>
+      <h3 className="h3Login">Log in</h3>
+      <div className="container">
+        <div className="formWrap">
+          <form>
+            <div className="formGroup">
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" id="password" />
+            </div>
+            <Button type="submit">Log in</Button>
+            <p>
+              Don't have an account? <Link to="/register">Sign up</Link> now.
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
