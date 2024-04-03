3f5d282c85d1b34b8e521067fcee550a0a277941
[satuhalinen] <satuh9@gmail.com>
2024-04-03 09:00:52 +0300

    add Bootstrap content to SmallHeader

--- src/components/SmallHeader.jsx
+++ src/components/SmallHeader.jsx
@@ -1,5 +1,20 @@
-import { NavLink } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
-export default function SmallHeader() {
-  return <NavLink>Back to Calendars</NavLink>;
+function SmallHeader() {
+  return (
+    <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Nav className="me-auto">
+          <NavLink style={{ textDecoration: "none" }} to="/calendars">
+            Back to Calendars
+          </NavLink>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default SmallHeader;
