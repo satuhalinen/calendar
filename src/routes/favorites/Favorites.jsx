f17c9e61a19447d815ec6d0c57ea26d4d6f66515
BenPet <bendzuke@gmail.com>
2024-04-03 22:31:27 +0300

    favorites skeleton

--- src/routes/favorites/Favorites.jsx
+++ src/routes/favorites/Favorites.jsx
@@ -1,3 +1,69 @@
+import Card from "react-bootstrap/Card";
+import { NavLink } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import homeSymbol from "../../public/home.png";
+
 export default function Favorites() {
-  return <div>Favorites</div>;
+  return (
+    <>
+      <h3 className="h3Favorites">Favorites</h3>
+      <div className="cards">
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 1</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 2</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 3</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 4</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 5</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <NavLink style={{ textDecoration: "none" }}>
+            <Card.Img variant="top" src={homeSymbol} />
+          </NavLink>
+          <Card.Body>
+            <Card.Title>Calendar 6</Card.Title>
+            <Button variant="primary">Remove</Button>
+          </Card.Body>
+        </Card>
+        <Button variant="primary">Remove all</Button>
+      </div>
+    </>
+  );
 }
