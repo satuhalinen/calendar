8732126990e2413e6f45e2feb8cb86ecfa227e4b
Pushpa Gnyawali <75498254+Pushpa88431@users.noreply.github.com>
2024-04-04 10:16:38 +0300

    Calanders skeleton

--- src/routes/Calendars.jsx
+++ src/routes/Calendars.jsx
@@ -1,3 +1,23 @@
 export default function Calendars() {
-  return <div>Calendars</div>;
+  return (
+    <div>
+      <div>
+        <label>
+          Select free or paid
+          <option value="">Select free or paid</option>
+        </label>
+      </div>
+      <div>
+        <label>
+          Sort
+          <option value="">Select</option>
+          <option value="name">Name</option>
+          <option value="price">Price</option>
+        </label>
+      </div>
+      <div>
+        <h2>Calendars</h2>
+      </div>
+    </div>
+  );
 }
