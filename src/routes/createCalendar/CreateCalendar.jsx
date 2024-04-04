08a5babe1707a5503de2c8c98439e415b3d6bcbc
hendmissonen <hendmissonen@gmail.com>
2024-04-04 14:19:59 +0300

    accountSettings/css

--- src/routes/createCalendar/CreateCalendar.jsx
+++ src/routes/createCalendar/CreateCalendar.jsx
@@ -8,23 +8,23 @@ import "./createCalendar.css";
 export default function CreateCalendar() {
   const colorThemeOptions = [
     {
-      label: "Option1",
+      label: "Option 1",
       type: "color",
-      value: "#FF5733",
+      value: "#597773",
     },
     {
-      label: "Option2",
+      label: "Option 2",
       type: "color",
-      value: "#3366FF",
+      value: "#BD8048",
     },
     {
-      label: "Option3",
+      label: "Option 3",
       type: "image",
       value:
         "https://images.pexels.com/photos/6101961/pexels-photo-6101961.jpeg?auto=compress&cs=tinysrgb&dpr=2",
     },
     {
-      label: "Option4",
+      label: "Option 4",
       type: "image",
       value:
         "https://images.pexels.com/photos/158536/butterfly-common-blue-restharrow-polyommatus-icarus-158536.jpeg?auto=compress&cs=tinysrgb&dpr=2",
@@ -32,38 +32,34 @@ export default function CreateCalendar() {
   ];
 
   return (
-    <Row>
+    <Row className="main-row">
       <Col xs={2}>
         <Leftbar />
       </Col>
       <Col xs={10}>
         <Container className="text-center mx-auto">
-          <h1>Create a Calendar</h1>
+          <h1 className="header-crCAL">Create a Calendar</h1>
           <p className="para">Choose free or not</p>
           <Row>
-            <Form>
-              <div className="mb-3">
-                {["Option 1", "Option 2"].map((option, index) => (
-                  <Form.Check
-                    key={`radio-${index}`}
-                    inline
-                    label={option}
-                    name="group1"
-                    type="radio"
-                    id={`radioButton-${index}`}
-                  />
-                ))}
-              </div>
+            <Form className="crCAL-form">
+              {["Option 1", "Option 2"].map((option, index) => (
+                <Form.Check
+                  key={`radio-${index}`}
+                  inline
+                  label={option}
+                  name="group1"
+                  type="radio"
+                  id={`radioButton-${index}`}
+                  className="radioButton"
+                />
+              ))}
             </Form>
           </Row>
           <p className="para">Choose the color theme</p>
-          <Row>
+          <Row className="justify-content-center">
             {colorThemeOptions.map((option, index) => (
-              <Col key={`option-${index}`}>
-                <Container
-                  fluid
-                  className="text-center mx-auto colorTheme-container "
-                >
+              <Col key={`option-${index}`} xs={12} sm={6} md={4} lg={3}>
+                <div className="d-flex flex-column  align-items-center">
                   <Form>
                     <Form.Check
                       inline
@@ -71,49 +67,54 @@ export default function CreateCalendar() {
                       label={option.label}
                       type="radio"
                       id={`radioButton-${index}`}
+                      className="radioButton"
                     />
                   </Form>
                   {option.type === "image" ? (
-                    <Container>
-                      <img className="optionImg" src={option.value} alt={option.label} />
-                    </Container>
+                    <div>
+                      <img
+                        className="optionImg"
+                        src={option.value}
+                        alt={option.label}
+                      />
+                    </div>
                   ) : (
-                    <Container>
+                    <div className="crCAL-color-div">
                       <div
                         style={{
                           backgroundColor: option.value,
-                          width: "150px",
-                          height: "150px",
+                          width: "184px",
+                          height: "123px",
                           marginTop: "5px",
-                          marginLeft: "30px",
+                          borderRadius: "5px",
                         }}
                       ></div>
-                    </Container>
+                    </div>
                   )}
-                </Container>
+                </div>
               </Col>
             ))}
           </Row>
-          <p className="para numbers">Number of hatches</p>
+          <p className="para ">Number of hatches</p>
           <Row>
-            <Form>
-              <div className="mb-3">
-                {["24", "28", "30", "31"].map((option, index) => (
-                  <Form.Check
-                    key={`radio-${index}`}
-                    inline
-                    label={option}
-                    name="group1"
-                    type="radio"
-                    id={`radio-${index}`}
-                    style={{ paddingRight: "100px" }}
-                  />
-                ))}
-              </div>
+            <Form className="crCAL-form2">
+              {["24", "28", "30", "31"].map((option, index) => (
+                <Form.Check
+                  key={`radio-${index}`}
+                  inline
+                  label={option}
+                  name="group1"
+                  type="radio"
+                  id={`radio-${index}`}
+                  className="radioButton"
+                />
+              ))}
             </Form>
           </Row>
 
-          <Button className="button">Create</Button>
+          <Button variant="light" type="submit" className="crCAL-button">
+            Create
+          </Button>
         </Container>
       </Col>
     </Row>
