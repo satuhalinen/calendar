ffd87b8e39ad6768a20375e03e2d39a91643f0b3
BenPet <bendzuke@gmail.com>
2024-04-09 17:40:55 +0300

    added register firebase logic

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
