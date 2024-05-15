53121ed0ccb6dc1d8056f4882b842066ff0e9c01
[satuhalinen] <satuh9@gmail.com>
2024-05-15 21:32:13 +0300

    make back button functional in modify old calendar page

--- src/routes/modifyOldCalendar/ModifyOldCalendar.jsx
+++ src/routes/modifyOldCalendar/ModifyOldCalendar.jsx
@@ -1,208 +1,208 @@
 import EditOldHatch from "../../components/EditOldHatch.jsx";
 import "../../calendar.css";
-import { Card, NavLink } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {
-    collection,
-    getDocs,
-    getDoc,
-    serverTimestamp,
-    doc,
-    setDoc,
+  collection,
+  getDocs,
+  getDoc,
+  serverTimestamp,
+  doc,
+  setDoc,
 } from "firebase/firestore";
 import { db } from "../../auth/firebase";
 import {
-    fetchFromFirebase,
-    setAvailableAlternatives,
+  fetchFromFirebase,
+  setAvailableAlternatives,
 } from "../../store/alternativesSlice";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, NavLink } from "react-router-dom";
 import SmallHeader from "../../components/smallHeader/SmallHeader.jsx";
 
 function ModifyOldCalendar() {
-    const { id } = useParams();
-    console.log(id);
-    const backgroundColor = useSelector(
-        (state) => state.calendarStyling.selectedColor
-    );
+  const { id } = useParams();
+  console.log(id);
+  const backgroundColor = useSelector(
+    (state) => state.calendarStyling.selectedColor
+  );
 
-    const hatchColor = useSelector(
-        (state) => state.calendarStyling.selectedHatchColor
-    );
+  const hatchColor = useSelector(
+    (state) => state.calendarStyling.selectedHatchColor
+  );
 
-    const generatedImage = useSelector(
-        (state) => state.calendarStyling.generatedImage
-    );
+  const generatedImage = useSelector(
+    (state) => state.calendarStyling.generatedImage
+  );
 
-    const uploadedImage = useSelector(
-        (state) => state.calendarStyling.uploadedImage
-    );
+  const uploadedImage = useSelector(
+    (state) => state.calendarStyling.uploadedImage
+  );
 
-    const selectedImage = useSelector(
-        (state) => state.calendarStyling.selectedImage
-    );
+  const selectedImage = useSelector(
+    (state) => state.calendarStyling.selectedImage
+  );
 
-    const selectedFont = useSelector(
-        (state) => state.calendarStyling.selectedFont
-    );
+  const selectedFont = useSelector(
+    (state) => state.calendarStyling.selectedFont
+  );
 
-    const selectedHatchFontColor = useSelector(
-        (state) => state.calendarStyling.selectedHatchFontColor
-    );
+  const selectedHatchFontColor = useSelector(
+    (state) => state.calendarStyling.selectedHatchFontColor
+  );
 
-    const selectedHatchesNumber = useSelector(
-        (state) => state.calendarStyling.selectedHatchesNumber
-    );
+  const selectedHatchesNumber = useSelector(
+    (state) => state.calendarStyling.selectedHatchesNumber
+  );
 
-    const titleFont = useSelector(
-        (state) => state.calendarStyling.selectedTitleFont
-    );
+  const titleFont = useSelector(
+    (state) => state.calendarStyling.selectedTitleFont
+  );
 
-    const title = useSelector((state) => state.calendarStyling.inputValue);
+  const title = useSelector((state) => state.calendarStyling.inputValue);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const fetchAlternatives = async () => {
-        const colRef = collection(db, "categories");
-        const querySnapshot = await getDocs(colRef);
+  const fetchAlternatives = async () => {
+    const colRef = collection(db, "categories");
+    const querySnapshot = await getDocs(colRef);
 
-        const data = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
 
-        dispatch(setAvailableAlternatives(data));
-    };
+    dispatch(setAvailableAlternatives(data));
+  };
 
-    useEffect(() => {
-        (async () => {
-            await fetchAlternatives();
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      await fetchAlternatives();
+    })();
+  }, []);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const saveHatchText = async () => {
-        if (calendarContent !== undefined) {
-            const docRef = doc(db, "calendars", id);
+  const saveHatchText = async () => {
+    if (calendarContent !== undefined) {
+      const docRef = doc(db, "calendars", id);
 
-            await setDoc(
-                docRef,
-                {
-                    content: calendarContent,
-                    calendarBackgroundColor: backgroundColor,
-                    calendarHatchColor: hatchColor,
-                    calendarImage: selectedImage,
-                    calendarFont: selectedFont,
-                    calendarHatchFontColor: selectedHatchFontColor,
-                    calendarHatchesNumber: selectedHatchesNumber,
-                    calendarTitleFont: titleFont,
-                    calendarTitle: title,
-                    calendarUploadedImage: uploadedImage,
-                    calendarGeneratedImage: generatedImage,
-                    createdAt: serverTimestamp(),
-                },
-                { merge: true }
-            );
+      await setDoc(
+        docRef,
+        {
+          content: calendarContent,
+          calendarBackgroundColor: backgroundColor,
+          calendarHatchColor: hatchColor,
+          calendarImage: selectedImage,
+          calendarFont: selectedFont,
+          calendarHatchFontColor: selectedHatchFontColor,
+          calendarHatchesNumber: selectedHatchesNumber,
+          calendarTitleFont: titleFont,
+          calendarTitle: title,
+          calendarUploadedImage: uploadedImage,
+          calendarGeneratedImage: generatedImage,
+          createdAt: serverTimestamp(),
+        },
+        { merge: true }
+      );
 
-            navigate(`/calendar/${id}`, { state: { from: true } });
-            console.log("Document updated with ID: ", id);
-        }
-    };
+      navigate(`/calendar/${id}`, { state: { from: true } });
+      console.log("Document updated with ID: ", id);
+    }
+  };
 
-    const calendarContent = useSelector(
-        (state) => state.alternatives.savedAlternatives
-    );
+  const calendarContent = useSelector(
+    (state) => state.alternatives.savedAlternatives
+  );
 
-    const backgroundImage = selectedImage || uploadedImage || generatedImage;
+  const backgroundImage = selectedImage || uploadedImage || generatedImage;
 
-    const fetchAlternativesFromFirebase = async () => {
-        const docRef = doc(db, "calendars", id);
-        const docSnapshot = await getDoc(docRef);
-        if (docSnapshot.exists()) {
-            const data = docSnapshot.data().content;
-            if (data !== undefined) {
-                dispatch(fetchFromFirebase(data));
-            }
-        }
-    };
+  const fetchAlternativesFromFirebase = async () => {
+    const docRef = doc(db, "calendars", id);
+    const docSnapshot = await getDoc(docRef);
+    if (docSnapshot.exists()) {
+      const data = docSnapshot.data().content;
+      if (data !== undefined) {
+        dispatch(fetchFromFirebase(data));
+      }
+    }
+  };
 
-    useEffect(() => {
-        (async () => {
-            await fetchAlternativesFromFirebase();
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      await fetchAlternativesFromFirebase();
+    })();
+  }, []);
 
-    return (
-        <>
-            <SmallHeader />
-            <div className="editCalendar">
-                <div className="editCalendarSections">
-                    <Card
-                        style={{
-                            margin: "1.5% 0",
-                            backgroundImage: backgroundImage
-                                ? `url(${backgroundImage})`
-                                : "none",
-                            backgroundColor: backgroundColor,
-                            backgroundSize: "cover",
-                            boxShadow: "0px 0px 5px 0px #00000059",
-                            border: "none",
-                        }}
-                    >
-                        <Card.Title
-                            style={{
-                                textAlign: "center",
-                                margin: "3% 0% 0% 0%",
-                                fontFamily: titleFont,
-                                color: selectedHatchFontColor,
-                            }}
-                        >
-                            <p className="editCalendarTitle">{title}</p>
-                        </Card.Title>
-                        <div className="calendar">
-                            {Array.from({ length: selectedHatchesNumber || 31 }).map(
-                                (_, i) => (
-                                    <EditOldHatch key={i} number={i + 1} />
-                                )
-                            )}
-                        </div>
-                    </Card>
-                </div>
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <NavLink
-                        style={{
-                            width: "30%",
-                            margin: "0% 0% 2% 0%",
-                            backgroundColor: "#BA6C2C",
-                            color: "#FFFAF7",
-                            border: "none",
-                            textDecoration: "none",
-                        }}
-                        className="backToCreateCalendarButton"
-                        to="/create-calendar"
-                    >
-                        Back
-                    </NavLink>
-
-                    <NavLink
-                        onClick={saveHatchText}
-                        style={{
-                            justifySelf: "center",
-                            backgroundColor: "#BA6C2C",
-                            color: "#FFFAF7",
-                            border: "none",
-                            margin: "0% 0% 2% 0%",
-                            textDecoration: "none",
-                        }}
-                        className="createCalendarButton"
-                        to="#"
-                    >
-                        Create calendar
-                    </NavLink>
-                </div>
+  return (
+    <>
+      <SmallHeader />
+      <div className="editCalendar">
+        <div className="editCalendarSections">
+          <Card
+            style={{
+              margin: "1.5% 0",
+              backgroundImage: backgroundImage
+                ? `url(${backgroundImage})`
+                : "none",
+              backgroundColor: backgroundColor,
+              backgroundSize: "cover",
+              boxShadow: "0px 0px 5px 0px #00000059",
+              border: "none",
+            }}
+          >
+            <Card.Title
+              style={{
+                textAlign: "center",
+                margin: "3% 0% 0% 0%",
+                fontFamily: titleFont,
+                color: selectedHatchFontColor,
+              }}
+            >
+              <p className="editCalendarTitle">{title}</p>
+            </Card.Title>
+            <div className="calendar">
+              {Array.from({ length: selectedHatchesNumber || 31 }).map(
+                (_, i) => (
+                  <EditOldHatch key={i} number={i + 1} />
+                )
+              )}
             </div>
-        </>
-    );
+          </Card>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <NavLink
+            to={`/modify-old-calendar-styling/${id}`}
+            style={{
+              width: "30%",
+              margin: "0% 0% 2% 0%",
+              backgroundColor: "#BA6C2C",
+              color: "#FFFAF7",
+              border: "none",
+              textDecoration: "none",
+            }}
+            className="backToCreateCalendarButton"
+          >
+            Back
+          </NavLink>
+
+          <NavLink
+            onClick={saveHatchText}
+            style={{
+              justifySelf: "center",
+              backgroundColor: "#BA6C2C",
+              color: "#FFFAF7",
+              border: "none",
+              margin: "0% 0% 2% 0%",
+              textDecoration: "none",
+            }}
+            className="createCalendarButton"
+            to="#"
+          >
+            Create calendar
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
 }
 export default ModifyOldCalendar;
